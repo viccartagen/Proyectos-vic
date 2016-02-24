eb777186e42997f76bf4d1a4059ff1985189bbbe
Vic Cartagena <vic@MacBook-Pro-de-MacBook.local>
2016-02-24 18:57:51 -0300

    actualización atrasada

---
+++ P 13/esPrimo.html
@@ -0,0 +1,18 @@
+function esPrimo(numero) {
+	for(var i = 2; i < numero; i++){
+		if(numero % i == 0) {
+			console.log("noPrimo");
+			return false;
+		}
+	}
+	return true;
+}
+class Primos{ 
+	public static;
+	void main(String arg[])
+		{ int i,j; boolean esPrimo; int rInicial=2;int rFinal = 200;
+		for(i = rInicial;i <= rFinal;i++) {
+			esPrimo=true;
+
+			for(j = 2;j < i;j++) {/o lo imprimo. System.out.println(i+"\n"); } } } } - See more at: http://usandojava.blogspot.cl/2010/12/hallar-numeros-primos-con-un-rango.html#sthash.tyOM9tjp.dpuf
+ x
---
+++ ejemplo1.js
@@ -0,0 +1,34 @@
+	var hola = 6;
+
+	for (var i=1 ; i <= hola ; i++) {
+		concole.log(i);
+	}
+	var chao = true;
+	while (chao) {
+		chao = false;
+		console.log(chao);
+	}
+var queEstaPasando = 5;
+
+if (queEstaPasando > 3) {
+	console.log("soy grande!");
+}
+else if (queEstaPasando == 0) {
+	console.log("soy nulo!");
+}
+else {
+	console.log("soy chico!");
+}
+var valor = 5;
+switch (valor) {
+	case 3:
+		console.log("aquí vale 3");
+		break;
+	case 5:
+		console.log("aquí valgo 5");
+		break;
+	default:
+		console.log("soy el resto");
+		break;
+
+}
